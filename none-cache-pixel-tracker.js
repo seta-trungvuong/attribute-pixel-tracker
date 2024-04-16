7c6f731878bf89e0ed4fd3f41784207bca077338
Vuong Trung <tvuong@veritone.com>
2024-04-16 17:50:48 +0700

    Update none-cache-pixel-tracker.js

--- none-cache-pixel-tracker.js
+++ none-cache-pixel-tracker.js
@@ -6,6 +6,6 @@
   veritoneAttributeScript.async = true;
   veritoneAttributeScript.type = 'application/javascript';
   veritoneAttributeScript.setAttribute("attribute-pixel-id", pixelId)
-  veritoneAttributeScript.src = `https://cdn.jsdelivr.net/gh/seta-trungvuong/attribute-pixel-tracker@main/attribute-pixel-tracker.js?v=${currentTimeStamp}`;
+  veritoneAttributeScript.src = `https://cdn.statically.io/gh/seta-trungvuong/attribute-pixel-tracker/develop/attribute-pixel-tracker.js?v=${currentTimeStamp}`;
   document.body.appendChild(veritoneAttributeScript);
 })();
