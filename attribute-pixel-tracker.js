95ac9123ad68d80b2b627cce8be5cc6857d26bb9
Vuong Trung <tvuong@veritone.com>
2024-04-19 16:58:48 +0700

    Update attribute-pixel-tracker.js

--- attribute-pixel-tracker.js
+++ attribute-pixel-tracker.js
@@ -4,10 +4,12 @@
     const currentTimeStamp = new Date().valueOf();
     const pageUrl = document.URL;
     const referrer = document.referrer;
-    const pixelImage = document.createElement('img')
+    const pixelImage = document.createElement('img');
     const imageContainer = document.getElementById("pixel-container");
-    pixelImage.id = "attribute-pixel-img"
-    pixelImage.referrerpolicy = "no-referrer-when-downgrade"
+    pixelImage.id = "attribute-pixel-img";
+    pixelImage.referrerpolicy = "no-referrer-when-downgrade";
+    pixelImage.width = 1;
+    pixelImage.height = 1;
     pixelImage.src =`https://p.veritone-ce.com/${pixelId}?ts=${currentTimeStamp}&url=${encodeURIComponent(pageUrl)}&ref=${encodeURIComponent(referrer)}`
     document.body.appendChild(pixelImage);
     console.log(document.getElementById("attribute-pixel-img").src)
